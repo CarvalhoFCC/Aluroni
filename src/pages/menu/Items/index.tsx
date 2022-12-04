f78c7aa1572bad47f2c75f9873055be4410d8a5e
Felipe Carrico <felipeccarrico@gmail.com>
2022-12-04 06:31:36 -0300

    style: single quote instead of double

--- src/pages/menu/Items/index.tsx
+++ src/pages/menu/Items/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
-import Item from "./Item";
-import menu from "./items.json";
-import styles from "./Items.module.scss";
+import { useEffect, useState } from 'react';
+import Item from './Item';
+import menu from './items.json';
+import styles from './Items.module.scss';
 
 interface Props {
 	search: string,
@@ -10,44 +10,44 @@ interface Props {
 }
 
 export default function Items({ filter, sort, search }: Props) {
-	const [list, setList] = useState(menu);
+  const [list, setList] = useState(menu);
 
-	function searchTest(title: string) {
-		const regex = new RegExp(search, "i");
+  function searchTest(title: string) {
+    const regex = new RegExp(search, 'i');
 
-		return regex.test(title);
-	}
+    return regex.test(title);
+  }
 
-	function filterTest(id: number) {
-		if(filter !== null) return filter === id;
+  function filterTest(id: number) {
+    if(filter !== null) return filter === id;
 
-		return true;
-	}
+    return true;
+  }
 
-	function sortList(newList: typeof menu) {
+  function sortList(newList: typeof menu) {
     switch(sort) {
-      case 'porcao': 
-        return newList.sort((a, b) => a.size > b.size ? 1 : -1);
-      case 'qtd_pessoas':
-        return newList.sort((a,b) => a.serving > b.serving ? 1 : -1);
-      case 'preco':
-        return newList.sort((a,b) => a.price > b.price ? 1 : -1);
-      default:
-        return newList; 
+    case 'porcao': 
+      return newList.sort((a, b) => a.size > b.size ? 1 : -1);
+    case 'qtd_pessoas':
+      return newList.sort((a,b) => a.serving > b.serving ? 1 : -1);
+    case 'preco':
+      return newList.sort((a,b) => a.price > b.price ? 1 : -1);
+    default:
+      return newList; 
     }
   }
 
-	useEffect(() => {
-		const newList = menu.filter(item => searchTest(item.title) && filterTest(item.category.id));
+  useEffect(() => {
+    const newList = menu.filter(item => searchTest(item.title) && filterTest(item.category.id));
 
-		setList(sortList(newList));
-	}, [search, filter, sort]);
+    setList(sortList(newList));
+  }, [search, filter, sort]);
 
-	return (
-		<div className={styles.items}>
-			{list.map((item) => (
-				<Item key={item.id} {...item} />
-			))}
-		</div>
-	);
+  return (
+    <div className={styles.items}>
+      {list.map((item) => (
+        <Item key={item.id} {...item} />
+      ))}
+    </div>
+  );
 }
--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,35 +1,35 @@
-import styles from "./Menu.module.scss";
-import { ReactComponent as Logo } from "assets/logo.svg"
-import Search from "./Search";
-import { useState } from "react";
-import Filters from "./Filters";
-import Order from "./Order";
-import Items from "./Items";
+import styles from './Menu.module.scss';
+import { ReactComponent as Logo } from 'assets/logo.svg';
+import Search from './Search';
+import { useState } from 'react';
+import Filters from './Filters';
+import Order from './Order';
+import Items from './Items';
 
 export default function Menu() {
-	const [search, setSearch] = useState("");
-	const [filter, setFilter] = useState<number | null>(null);
-	const [order, setOrder] = useState("");
+  const [search, setSearch] = useState('');
+  const [filter, setFilter] = useState<number | null>(null);
+  const [order, setOrder] = useState('');
 
-	return (
-		<main>
-			<nav className={styles.menu}>
-				<Logo />
-			</nav>
-			<header className={styles.header}>
-				<div className={styles.header__text}>
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
 					A casa do código e da massa
-				</div>
-			</header>
-			<section className={styles.foodMenu}>
-				<h3 className={styles.foodMenu__title}>Cardápio</h3>
-				<Search search={search} setSearch={setSearch} />
-				<div className={styles.foodMenu__filters}>
-					<Filters filter={filter} setFilter={setFilter} />
-					<Order order={order} setOrder={setOrder} />
-				</div>
-				<Items search={search} filter={filter} sort={order} />
-			</section>
-		</main>
-	)
+        </div>
+      </header>
+      <section className={styles.foodMenu}>
+        <h3 className={styles.foodMenu__title}>Cardápio</h3>
+        <Search search={search} setSearch={setSearch} />
+        <div className={styles.foodMenu__filters}>
+          <Filters filter={filter} setFilter={setFilter} />
+          <Order order={order} setOrder={setOrder} />
+        </div>
+        <Items search={search} filter={filter} sort={order} />
+      </section>
+    </main>
+  );
 }
